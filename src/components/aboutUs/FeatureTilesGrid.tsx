7152074b6c2baa71740bb2854edc396fd6434592
it21296796 <99311998+it21296796@users.noreply.github.com>
2025-05-05 18:58:09 +0530

    d

--- src/components/aboutUs/FeatureTilesGrid.tsx
+++ src/components/aboutUs/FeatureTilesGrid.tsx
@@ -5,7 +5,7 @@ export default function FeatureGrid() {
     <div className="bg-black p-4 md:p-8 w-full">
       <div className="flex flex-wrap gap-x-4 justify-center max-w-[1280px] mx-auto">
         {/* Tile 1 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[300px] lg:w-[280px] xl:w-[320px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[260px] lg:w-[260px] xl:w-[320px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile1.png" alt="App background" fill className="object-cover" />
           <div className="absolute inset-0 p-6 flex flex-col justify-center items-center">
             <div>
@@ -19,12 +19,12 @@ export default function FeatureGrid() {
         </div>
 
         {/* Tile 2 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[320px] md:w-[360px] lg:w-[340px] xl:w-[400px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[320px] md:w-[300px] lg:w-[300px] xl:w-[400px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile2.png" alt="Mobile app development" fill className="object-cover" />
         </div>
 
         {/* Tile 3 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[300px] lg:w-[280px] xl:w-[320px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[260px] lg:w-[260px] xl:w-[320px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile1.png" alt="Industries background" fill className="object-cover" />
           <div className="absolute inset-0 p-6 flex flex-col justify-center items-center">
             <div>
@@ -38,7 +38,7 @@ export default function FeatureGrid() {
         </div>
 
         {/* Tile 4 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[300px] lg:w-[280px] xl:w-[320px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[260px] lg:w-[260px] xl:w-[320px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile3.png" alt="Build something background" fill className="object-cover" />
           <div className="absolute inset-0 p-6 flex items-center justify-center">
             <p className="text-black text-lg md:text-xl font-[400] text-center" style={{ fontFamily: "Inter" }}>
@@ -48,7 +48,7 @@ export default function FeatureGrid() {
         </div>
 
         {/* Tile 5 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[300px] lg:w-[280px] xl:w-[320px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[260px] md:w-[260px] lg:w-[260px] xl:w-[320px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile1.png" alt="Customers background" fill className="object-cover" />
           <div className="absolute inset-0 p-6 flex flex-col justify-center items-center">
             <div>
@@ -62,7 +62,7 @@ export default function FeatureGrid() {
         </div>
 
         {/* Tile 6 */}
-        <div className="rounded-lg overflow-hidden relative mb-4 w-[320px] md:w-[360px] lg:w-[340px] xl:w-[400px] h-[260px] md:h-[300px] lg:h-[280px] xl:h-[320px]">
+        <div className="rounded-lg overflow-hidden relative mb-4 w-[320px] md:w-[300px] lg:w-[300px] xl:w-[400px] h-[260px] md:h-[260px] lg:h-[260px] xl:h-[320px]">
           <Image src="/FeatureTile4.png" alt="Mobile app development" fill className="object-cover" />
         </div>
       </div>
